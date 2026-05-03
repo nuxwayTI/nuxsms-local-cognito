ded78b3298beae89193b4339378e803bbc274803
nuxwayTI <110561894+nuxwayTI@users.noreply.github.com>
2026-05-03 17:58:57 -0400

    Update SCOPES to include 'phone' instead of 'profile'

--- app/config.py
+++ app/config.py
@@ -2,8 +2,12 @@ COGNITO_DOMAIN = "https://us-east-2rsijbyygt.auth.us-east-2.amazoncognito.com"
 REGION = "us-east-2"
 USER_POOL_ID = "us-east-2_rsIjByyGT"
 APP_CLIENT_ID = "6sdnf5j17rhtl6l2tp1l02oq21"
+
 CALLBACK_HOST = "localhost"
 CALLBACK_PORT = 8765
+
+# IMPORTANTE: HTTP (no https)
 CALLBACK_URL = "http://localhost:8765/callback"
 LOGOUT_URL = "http://localhost:8765/logout"
-SCOPES = "openid email profile"
+
+SCOPES = "openid email phone"
